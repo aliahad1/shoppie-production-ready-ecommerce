0e52fa22769d5b1b7fdc2cbfc52f690c3414da20
Ali <90032081+iamAliAhad@users.noreply.github.com>
2023-01-05 10:21:24 +0800

    assets and ads

---
+++ shoppie/components/header/Ad.js
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss'
+import Link from 'next/link'
+
+function Ad() {
+    return (
+        <Link href="/browse">
+            <div className={styles.ad}>
+
+            </div>
+        </Link>
+    )
+}
+
+export default Ad
--- shoppie/pages/_app.js
+++ shoppie/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import store from '../store'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -8,10 +9,18 @@ let persistor = persistStore(store)
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Shoppie</title>
+        <meta name="description" content="Online shopping service" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   )
 }
